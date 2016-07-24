a8b14773e991f8884239afb354b9be7c15b9ee64
Mark Lee <marklee.lhs@gmail.com>
2016-07-24 15:13:40 -0400

    Update config file, wip pnu

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ import json
 def return_config(filename='config.json'):
     with open(filename, 'r') as json_file:
         config = json.load(json_file)
-
     return config
 
-config = return_config()
+JSONConfig = return_config()
