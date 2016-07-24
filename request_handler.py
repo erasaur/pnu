ac74ea0acd7917a3551e3f0c3dc897fa8f6bc31a
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-24 05:26:47 -0400

    implement api requests to retrieve new unread email info

--- request_handler.py
+++ request_handler.py
@@ -1,6 +1,8 @@
 import threading
 
+from requester import PnuRequest
 from runnable import PnuRunnable
+from JSONConfig import config
 
 # TODO
 # handler should be responsible for parsing incoming requests and updating the
@@ -10,21 +12,23 @@ from runnable import PnuRunnable
 # sms api, etc. but should expose an interface that is agnostic to the method by
 # which we are obtaining data (gmail, sms, and so on)
 
-# how often to check for new requests
-UPDATE_INTERVAL = 5.0 # seconds
-
 class PnuRequestHandler (PnuRunnable):
     def __init__ (self, loop=None, store=None):
         if loop is None or store is None:
             raise ValueError('missing loop or store')
 
-        super().__init__(UPDATE_INTERVAL)
+        super().__init__(config['request_handler']['update_interval'])
 
-        # TODO initialize Gmail api
+        self._requester = PnuRequest()
 
         self._loop = loop
         self._store = store
 
     def update (self):
-        # query using Gmail api, write changes to store
-        pass
+        # query using PnuRequest api, write changes to store
+        for new_msg in self._requester.run():
+            # the value is a tuple right now so that we can contain the location
+            # and pokemon_wanted all in one collection
+            self._store.set(new_msg['phone_number'], (new_msg['location'],
+                    new_msg['pokemon_wanted']))
+
