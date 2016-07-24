c303ba6554af25e6c326e15b66cf46e88db8f796
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-24 04:14:20 -0400

    add easier way to load config file

---
+++ JSONConfig.py
@@ -0,0 +1,9 @@
+import json
+
+def return_config(filename='config.json'):
+    with open(filename, 'r') as json_file:
+        config = json.load(json_file)
+
+    return config
+
+config = return_config()
