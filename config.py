fc9d3727eb8a96b8d8ab7ee3a1742fb95fc5e2a8
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-24 16:42:02 -0400

    updating config file to handle public/private data

--- config.py
+++ config.py
@@ -1,8 +1,39 @@
+from os import environ, path
 import json
 
-def return_config(filename='config.json'):
-    with open(filename, 'r') as json_file:
-        config = json.load(json_file)
-    return config
 
-JSONConfig = return_config()
+class Configurable:
+
+    def __init__(self, config_path=None):
+        if config_path is None:
+            self.config_path = environ.get(
+                'PNU_CONFIG',
+                path.join(path.dirname(__file__), 'etc/config.json'),
+            )
+        else:
+            self.config_path = config_path
+
+
+    def return_config(self):
+        with open(self.config_path, 'r') as f:
+            return json.load(f)
+
+
+class Config(Configurable):
+    """ returns public data utilized by pnu """
+
+    def __init__(self):
+        super().__init__('etc/config.json')
+
+    def load_config(self):
+        return super().return_config()
+
+
+class PrivateConfig(Configurable):
+    """ return sensitive data utilized by pnu """
+
+    def __init__(self):
+        super().__init__('private/config.json')
+
+    def load_config(self):
+        return super().return_config()
