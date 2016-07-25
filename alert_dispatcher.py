9d6ef0e9995ed920392a821619c267526f184b62
Mark Lee <marklee.lhs@gmail.com>
2016-07-24 23:48:39 -0400

    Update dispatcher to use smtp

--- alert_dispatcher.py
+++ alert_dispatcher.py
@@ -2,7 +2,14 @@
 # dispatcher should be responsible for batch-sending alerts to a list of phone
 # numbers
 
+from alerter import smpt
+
 class PnuAlertDispatcher (PnuHTTPClient):
     def dispatch (self, data):
         # for each number in data, dispatch data[number]
-        pass
+        for poke_list, phone_num_list in data.iteritems():
+            smtp.send_message({
+                "phone_number": phone_num_list,
+                "pokemon_wanted": poke_list,
+                "link": "example.com"
+            })
