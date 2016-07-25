1f80955e33387f2f1c16b6050bc89d24ebff6730
Mark Lee <marklee.lhs@gmail.com>
2016-07-24 23:52:30 -0400

    PnuDataStore now a singleton, move alert logic to poke api

--- pnu.py
+++ pnu.py
@@ -11,17 +11,11 @@ class Pnu (PnuRunnable):
     def __init__ (self):
         super().__init__(pub_config["pnu"]["update_interval"])
 
-        # initialize data store
-        data_store = PnuDataStore(
-            host=pub_config["pnu"]["store_host"],
-            port=pub_config["pnu"]["store_port"])
-
         # initialize event loop
         loop = asyncio.get_event_loop()
 
         # initialize api clients
         self._loop = loop
-        self._store = data_store
 
         with aiohttp.ClientSession(loop=loop) as session:
             self._poke_api = PnuPokeApi(session=session)
@@ -34,25 +28,7 @@ class Pnu (PnuRunnable):
         self._loop.run_forever()
 
     def update (self):
-        # query poke api and store new (just appeared) pokemon in a list
-        curr_pokes = self._poke_api.get_pokemon()
-
-        # run through each "hotspot" location and check if any new pokes are in
-        # a user's whitelist
-        users = self._data_store.list() # list of all users
-        alerts = [] # list of alerts to send out
-
-        for user in users:
-            pokemon = [] # pokemon that user might be interested in
-            for poke in curr_pokes:
-                if self._poke_api.is_nearby(poke["location"], user["location"]):
-                    pokemon.append(poke)
-
-            if len(pokemon) > 0:
-                alerts.append({
-                    "phone": user["phone"],
-                    "pokemon": pokemon
-                })
+        alerts = self._poke_api.get_new_pokemon()
 
         # send alerts with dispatcher
         self._dispatcher.send(alerts)
