98042bb5f7609f3cf300773b45b4f15314ca53df
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-24 20:13:55 -0400

    sms switches to mms when msg >= 159

--- alerter.py
+++ alerter.py
@@ -7,6 +7,12 @@ from email.mime.text import MIMEText
 class Alert:
 
     SUBJECT = "Pokemon Alert!"
+    SMS_CARRIERS = ["message.alltel.com", "txt.att.net", "myboostmobile.com",
+                    "messaging.sprintpcs.com", "tmomail.net", "email.uscc.net",
+                    "vtext.com", "vmobl.com"]
+    MMS_CARRIERS = ["mms.alltelwireless.com", "mms.att.net", "myboostmobile.com",
+                    "pm.sprint.com", "tmomail.net", "mms.uscc.net", "vzwpix.com",
+                    "vmpix.com"]
 
     def __init__(self):
         """ perform preliminary actions for sending email via SMTP """
@@ -25,11 +31,19 @@ class Alert:
         pokemon_wanted = self.list_to_str(info['pokemon_wanted'])
         a_an = self.format_plural_a_an(info['pokemon_wanted'])
 
-        msg = MIMEText("There's {a_an} {pokemon} near you! Go catch 'em!\n{link}"
+        msg = MIMEText("There's {a_an} {pokemon} near you! Go catch 'em!\n{link}\n\n"
                 .format(a_an=a_an, pokemon=pokemon_wanted, link=info['link']))
+
+        # if the message to send is over 160 characters, send it via the mms
+        # gateway instead of sms. The minus 2 is for parenthesis that get added
+        # to the subject part of the message being sent.
+        print(len(msg.get_payload()))
+        if (len(msg.get_payload()) > (159 - 2)):
+            info['phone_number'] = self.sms_to_mms(info['phone_number'])
+
         msg['To'] = info['phone_number']
         msg['From'] = private_config['gmail']['username']
-        msg['Subject'] = "Pokemon Alert!"
+        msg['Subject'] = self.SUBJECT
 
         return msg.as_string()
 
@@ -54,13 +68,24 @@ class Alert:
 
         return str_of_poke
 
+    def sms_to_mms(self, phone_number):
+        """ converts the sms carrier gateway to mms carrier gateway """
+        num, ext = phone_number.split('@')
+
+        for i, carrier in enumerate(self.SMS_CARRIERS):
+            if ext == carrier:
+                ext = self.MMS_CARRIERS[i]
+
+        return num + "@" + ext
+
     def send_message(self, info):
         """ sends a text message alert to the specified user
         Args:
             info (dictionary)
         """
 
+        msg = self.build_message(info)
         self.smtp.sendmail(private_config['gmail']['username'],
-                [info['phone_number']], self.build_message(info))
+                [info['phone_number']], msg)
 
 smtp = Alert()
