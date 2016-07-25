e41b21f3fa89a12f5e076376ae849a8546202b9a
Mark Lee <marklee.lhs@gmail.com>
2016-07-25 00:45:38 -0400

    Return status as well as response

--- http_client.py
+++ http_client.py
@@ -3,14 +3,14 @@ import asyncio, aiohttp
 class PnuHTTPClient ():
     def __init__ (self, session=None):
         if session is None:
-            raise ValueError('missing session')
+            raise ValueError("missing session")
         self._session = session
 
     async def get_json (self, url):
         async with self._session.get(url) as resp:
             try:
                 res = await resp.json()
-            except:
-                print(resp.status)
+            except Exception as e:
+                logging.info("failed decoding response, got exception: {}".format(e))
                 res = {}
-            return res
+            return { "status": resp.status, "res": res }
