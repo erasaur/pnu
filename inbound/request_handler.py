40c278b1d0e41dc08b6c2fa125c638a0b3ff446f
Mark Lee <marklee.lhs@gmail.com>
2016-07-25 01:41:27 -0400

    Update request handler to use User model

--- inbound/request_handler.py
+++ inbound/request_handler.py
@@ -13,22 +13,19 @@ from pnu.inbound.requester import PnuRequest
 # which we are obtaining data (gmail, sms, and so on)
 
 class PnuRequestHandler (PnuRunnable):
-    def __init__ (self, loop=None, store=None):
-        if loop is None or store is None:
-            raise ValueError('missing loop or store')
+    def __init__ (self, store=None):
+        if store is None:
+            raise ValueError("missing store")
 
-        super().__init__(pub_config['request_handler']['update_interval'])
+        super().__init__(pub_config["request_handler"]["update_interval"])
 
         self._requester = PnuRequest()
-
-        self._loop = loop
         self._store = store
 
     async def update (self):
         # query using PnuRequest api, write changes to store
         for new_msg in self._requester.run():
-            # the value is a tuple right now so that we can contain the location
-            # and pokemon_wanted all in one collection
-            self._store.set(new_msg['phone_number'], {new_msg['location'],
-                    new_msg['pokemon_wanted']})
+            # each new message represents a new user
+            user = User(new_msg)
+            self._store.update(user.get_phone_number(), user.get_json())
 
