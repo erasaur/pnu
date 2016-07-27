e13c95c46e4e44a9eda41b6bca79d6bad7ea0a16
Mark Lee <marklee.lhs@gmail.com>
2016-07-27 01:33:19 -0400

    Update pokemon model to store pokemon names

--- pnu/models/pokemon.py
+++ pnu/models/pokemon.py
@@ -1,4 +1,5 @@
 from pnu.models.base import Base
+from pnu.etc.constants import POKEMON_ID_TO_NAME
 
 class Pokemon (Base):
     def load_args (self, pokemonId, lat, lon, expiration_time):
@@ -8,6 +9,14 @@ class Pokemon (Base):
             longitude=lon,
             expiration_time=expiration_time
         )
+
+    def load_json (self, data):
+        super().load_json(data)
+        
+        try:
+            self.pokemon_name = POKEMON_ID_TO_NAME[self.pokemonId]
+        except:
+            self.pokemon_name = None
 
     def get_json (self):
         return {
@@ -25,6 +34,9 @@ class Pokemon (Base):
             self.longitude,
             self.expiration_time
         )
+
+    def get_name (self):
+        return self.pokemon_name
 
     def get_id (self):
         return self.pokemonId
