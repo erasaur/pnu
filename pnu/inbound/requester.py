786874d38625028f400c74f2990dd28c0a7715dc
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-28 02:41:27 -0400

    captures singular pokemon in list now

--- pnu/inbound/requester.py
+++ pnu/inbound/requester.py
@@ -15,7 +15,7 @@ logging = logging.getLogger(__name__)
 
 class PnuRequest:
     location_regex = re.compile("[@|\(\=](?P<lat>[\d|.|-]*)?,(?P<lon>[\d|.|-]*)?[,|&|\)]", re.IGNORECASE)
-    pokemon_regex = re.compile("pokemon\s*[a-z]*:\s*((([a-z]*-*[a-z]*),\s[a-z]*-*[a-z]*){0,4})", re.IGNORECASE)
+    pokemon_regex = re.compile("pokemon\s*[a-z]*:\s*((([a-z]*-*[a-z]*)[,| ]*[a-z]*-*[a-z]*){0,5})", re.IGNORECASE)
 
     split_regex = re.compile(', |; | |,')
     stop_regex = re.compile('stop', re.IGNORECASE)
