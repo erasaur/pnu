3b82b610e9618d65b2bc4f0e694bc7d1a2566eeb
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-07-28 01:32:15 -0400

    fix style errors, change __init__ to accept alert

--- pnu/outbound/response.py
+++ pnu/outbound/response.py
@@ -1,6 +1,7 @@
 from email.mime.text import MIMEText
-from pnu.config import private_config
+from pnu.config import private_config, constants
 from pnu.etc import constants
+from pnu.models.alert import Alert
 
 import logging
 logging = logging.getLogger(__name__)
@@ -10,18 +11,21 @@ class BuildResponse:
 
     SUBJECT = "Pokemon Alert!"
     SMS_CARRIERS = ["message.alltel.com", "txt.att.net", "myboostmobile.com",
-            "messaging.sprintpcs.com", "tmomail.net", "email.uscc.net",
-            "vtext.com", "vmobl.com"]
-    MMS_CARRIERS = ["mms.alltelwireless.com", "mms.att.net", "myboostmobile.com",
-            "pm.sprint.com", "tmomail.net", "mms.uscc.net", "vzwpix.com",
-            "vmpix.com"]
+                    "messaging.sprintpcs.com", "tmomail.net",
+                    "email.uscc.net", "vtext.com", "vmobl.com"]
+    MMS_CARRIERS = ["mms.alltelwireless.com", "mms.att.net",
+                    "myboostmobile.com", "pm.sprint.com", "tmomail.net",
+                    "mms.uscc.net", "vzwpix.com", "vmpix.com"]
 
-    def __init__(self, user, link = None, pokemon = None):
-        if isinstance(user, list):
+    def __init__(self, user):
+        self.link = None
+        if isinstance(user, Alert):
+            alert = user
             self.status = constants.ACTIVE
             # user in this case is a list of users' phone numbers
-            self.to = user
-            self.pokemon_wanted = pokemon
+            self.to = alert.get_phone_numbers()
+            self.pokemon_wanted = alert.get_pokemon_names()
+            self.link = alert.get_short_link()
             # spoofed since we have a list of users we presume to be
             # already valid
             self.location = True
@@ -31,14 +35,12 @@ class BuildResponse:
             self.pokemon_wanted = user.get_pokemon_wanted()
             self.location = (user.get_lat() or user.get_lon())
 
-        self.link = link
-
     def _make_enroll_msg(self):
         logging.info("Sending WELCOME message")
-        msg = ("PAUSE - temporarily suspend alerts\nRESUME "
-                + "- resume previous alerts\nSTOP - quit receiving alerts"
-                + "\nTo activate your user, respond in the form of 'Pokemon wanted: "
-                + "poke1, poke2, poke3' up to five pokemon")
+        msg = ("PAUSE - temporarily suspend alerts\nRESUME " +
+               "- resume previous alerts\nSTOP - quit receiving alerts" +
+               "\nTo activate your user, respond in the form of \"Pokemon"
+               "wanted: poke1, poke2, poke3\" up to five pokemon")
         self.to = self._check_len_of_msg(msg)
         return msg
 
@@ -60,13 +62,13 @@ class BuildResponse:
 
     def _make_no_location_msg(self):
         logging.info("Sending no location message")
-        return ("It does not look like we have your location. Please send us "
-                + "your location and try your request again.")
+        return ("It does not look like we have your location. Please send " +
+                "us your location and try your request again.")
 
     def _make_reenroll_msg(self):
         logging.info("Sending no re-enroll message")
-        return ("There seems to be a miscommunication. Please try re-enrolling by"
-                + " sending us your location first.")
+        return ("There seems to be a miscommunication. Please try " +
+                "re-enrolling by sending us your location first.")
 
     def _make_active_msg(self):
         """ returns the text message to be sent to the receiver
@@ -79,10 +81,9 @@ class BuildResponse:
         """
         logging.info("Sending ACTIVE message")
         self.pokemon_wanted = self.poke_list_to_str()
-        a_an = self.format_plural_a_an()
-        msg = MIMEText("There's {a_an} {pokemon} near you! Go catch 'em!\n{link}\n"
-                .format(a_an=a_an, pokemon=self.pokemon_wanted,
-                        link=self.link))
+        msg = MIMEText("There's a wild {pokemon} near you! Go catch 'em!\n" +
+                       "{link}\n".format(pokemon=self.pokemon_wanted,
+                                         link=self.link))
 
         # if the message to send is over 160 characters, send it via the mms
         # gateway instead of sms. The minus 2 is for parenthesis that get added
@@ -117,27 +118,22 @@ class BuildResponse:
 
         return num + "@" + ext
 
-    def format_plural_a_an(self):
-        """ returns either a or an depending on the 1st letter """
-        if self.pokemon_wanted[0][0].lower() in ['a', 'e', 'i', 'o', 'u']:
-            return 'an'
-
-        return 'a'
-
     def poke_list_to_str(self):
-        """ creates a string from the list of pokemon wanted 
+        """ creates a string from the list of pokemon wanted
         Args:
             pokemon_wanted (list)
         Returns:
             string A string of pokemon such as "poke1, poke2, and poke3"
         """
         str_of_poke = self.pokemon_wanted[0]
-        logging.info("List of pokemon_wanted is: {}".format(self.pokemon_wanted))
+        logging.info("List of pokemon_wanted is: {}".format(
+                self.pokemon_wanted))
 
         if len(self.pokemon_wanted) > 1:
-            str_of_poke = (', '.join(self.pokemon_wanted[:-1]) + ", and "
-                    + self.pokemon_wanted[-1])
-            logging.info("List of pokemon from string is: {}".format(str_of_poke))
+            str_of_poke = (", ".join(self.pokemon_wanted[:-1]) +
+                           ", and {}".format(self.pokemon_wanted[-1]))
+            logging.info("List of pokemon from string is: {}"
+                         .format(str_of_poke))
 
         return str_of_poke
 
@@ -150,10 +146,13 @@ class BuildResponse:
             RESUME message, or ACTIVE
         """
         # new user, no pokemon listed
-        if self.status == 'ENROLL':
+        if self.status == constants.ACTIVE:
+            return self._make_active_msg(), self.to
+
+        elif self.status == constants.ENROLL:
             return self._make_enroll_msg(), self.to
 
-        elif self.status == 'RESUME':
+        elif self.status == constants.RESUME:
             if not self.location:
                 return self._make_no_location_msg(), self.to
             elif not self.pokemon_wanted:
@@ -161,7 +160,7 @@ class BuildResponse:
             else:
                 return self._make_resume_msg(), self.to
 
-        elif self.status == 'PAUSE':
+        elif self.status == constants.PAUSE:
             if not self.location:
                 return self._make_no_location_msg(), self.to
             elif not self.pokemon_wanted:
@@ -169,16 +168,11 @@ class BuildResponse:
             else:
                 return self._make_pause_msg(), self.to
 
-        elif self.status == 'STOP':
+        elif self.status == constants.STOP:
             return self._make_stop_msg(), self.to
 
-        elif self.status == 'ACTIVE':
-            if not self.location:
-                return self._make_no_location_msg(), self.to
-            elif self.pokemon_wanted:
-                return self._make_active_msg(), self.to
-
-        logging.error("Type of message is undetermined. Here's some data to help")
+        logging.error("Type of message is undetermined. Here's some data to " +
+                      "help")
         logging.error("Pokemon wanted: {}".format(self.pokemon_wanted))
         logging.error("Status of user: {}".format(self.status))
         return self._make_reenroll_msg(), self.to
