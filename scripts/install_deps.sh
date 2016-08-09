ce8d9210a152d027685aa7830bb51fa3417c017e
Mark Lee <marklee.lhs@gmail.com>
2016-08-09 19:45:27 -0400

    Provide option for installing pip automatically

--- scripts/install_deps.sh
+++ scripts/install_deps.sh
@@ -15,11 +15,11 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
   return 1
 fi
 
-# need pip to be installed
-if [ -x "$(command -v pip)" ]; then
-  echo -e "${GREEN}pip is installed${NC}"
+# need python3.5
+if [ -x "$(command -v python3.5)" ]; then
+  echo -e "${GREEN}python3.5 is installed${NC}"
 else
-  echo -e "${RED}$MISSING_MSG: pip${NC}"
+  echo -e "${RED}$MISSING_MSG: python3.5${NC}"
   return 1
 fi
 
@@ -76,6 +76,45 @@ else
   fi
 fi
 
+# need pip to be installed
+if [ -x "$(command -v pip)" ]; then
+  echo -e "${GREEN}pip is installed${NC}"
+else
+  echo -e "${RED}$MISSING_MSG: pip${NC}"
+  # mac os
+  if [ "$(uname)" == "Darwin" ]; then
+    # only attempt to download for them if brew is installed
+    if [ -x "$(command -v brew)" ]; then
+      read -p "$DOWNLOAD_MSG" -n 1 -r
+      echo
+      if [[ $REPLY =~ ^[Yy]$ ]]; then
+        brew install python3
+      else
+        echo $EXITING
+        return
+      fi
+    else
+      echo $EXITING
+      return 1
+    fi
+
+  # linux
+  elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    read -p "$DOWNLOAD_MSG" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+      sudo apt-get update
+      sudo apt-get install python-pip
+    else
+      echo $EXITING
+      return 1
+    fi
+  else
+    echo $EXITING
+    return 1
+  fi
+fi
+
 # need virtualenv to be installed
 if [ -x "$(command -v virtualenv)" ]; then
   echo -e "${GREEN}virtualenv is installed${NC}"
@@ -89,14 +128,6 @@ else
     echo "$EXITING_MSG"
     return 1
   fi
-fi
-
-# need python3.5
-if [ -x "$(command -v python3.5)" ]; then
-  echo -e "${GREEN}python3.5 is installed${NC}"
-else
-  echo -e "${RED}$MISSING_MSG: python3.5${NC}"
-  return 1
 fi
 
 # setup virtualenv
