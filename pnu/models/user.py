808df2acfc874f3b836c053d8512fc6af3fbcda2
Cameron Gagnon <cameron.gagnon@gmail.com>
2016-08-10 01:56:07 -0400

    add error data to user model

--- pnu/models/user.py
+++ pnu/models/user.py
@@ -3,20 +3,25 @@ import logging
 logging = logging.getLogger(__name__)
 
 class User (Base):
-    def load_args (self, phone_number, pokemon_wanted, lat, lon, status):
+
+
+    def load_args(self, phone_number, pokemon_wanted, lat, lon, status,
+                  error_data=None):
         self.load_json({
             "phone_number": phone_number,
             "pokemon_wanted": pokemon_wanted,
             "latitude": lat,
             "longitude": lon,
             "status": status,
-            "last_notif": last_notif
+            "last_notif": last_notif,
+            "error_data": error_data
         })
 
-    def load_json (self, data):
+    def load_json(self, data):
         try:
             self.phone_number = data.get("phone_number")
             self.status = data.get("status")
+            self.error_data = data.get("error_data")
 
             # returns None if key doesn't exist
             self.pokemon_wanted = data.get("pokemon_wanted")
@@ -31,6 +36,7 @@ class User (Base):
                 self.lon = data.get("longitude")
             else:
                 self.lat = self.lon = None
+
         except KeyError as e:
             logging.info("Error loading user model: {}".format(e))
             self.phone_number = None
@@ -38,8 +44,9 @@ class User (Base):
             self.pokemon_wanted = None
             self.lat = self.lon = None
             self.last_notif = None
+            self.error_data = None
 
-    def get_json (self):
+    def get_json(self):
         location = None
         if self.lat and self.lon:
             location = {
@@ -52,50 +59,54 @@ class User (Base):
             "pokemon_wanted": self.pokemon_wanted,
             "location": location,
             "status": self.status,
-            "last_notif": self.last_notif
+            "last_notif": self.last_notif,
+            "error_data": self.error_data
         }
 
-    def get_lat (self):
+    def get_lat(self):
         return self.lat
 
-    def get_lon (self):
+    def get_lon(self):
         return self.lon
 
-    def get_pokemon_wanted (self):
+    def get_pokemon_wanted(self):
         return self.pokemon_wanted
 
-    def get_phone_number (self):
+    def get_phone_number(self):
         return self.phone_number
 
-    def get_status (self):
+    def get_status(self):
         return self.status
 
-    def set_status (self, status):
+    def set_status(self, status):
         self.status = status
+
+    def get_errors(self):
+        return self.error_data
 
     def is_location_set(self):
         return self.get_lat() is not None and self.get_lon() is not None
 
-    def is_pokemon_wanted_set (self):
+    def is_pokemon_wanted_set(self):
         pw = self.get_pokemon_wanted()
         return isinstance(pw, list) and len(pw) > 0
 
-    def is_active (self):
+    def is_active(self):
         return self.is_location_set() and self.is_pokemon_wanted_set()
 
-    def get_last_notif (self):
+    def get_last_notif(self):
         return self.last_notif
 
     # returns the latest expiration time (i.e newest copy) of
     # the poke that the user has seen so far
-    def get_last_notif_for_poke (self, poke):
+    def get_last_notif_for_poke(self, poke):
         # convert to string because dict keys are strings
         poke_id_str = str(poke.get_id())
         if not isinstance(self.last_notif, dict):
             return None
         return self.last_notif.get(poke_id_str)
 
-    def set_last_notif_for_poke (self, poke):
+    def set_last_notif_for_poke(self, poke):
         if not isinstance(self.last_notif, dict):
             self.last_notif = {}
 
@@ -106,7 +117,7 @@ class User (Base):
         if last_notif is None or expiration_time > last_notif:
             self.last_notif[poke_id_str] = expiration_time
 
-    def should_be_alerted (self, poke):
+    def should_be_alerted(self, poke):
         # user should be alerted of pokemon if:
         # - the pokemon is one that the user wants
         # - it's a new appearance of the pokemon
@@ -118,17 +129,17 @@ class User (Base):
         if not isinstance(wanted, list):
             wanted = []
 
-        return (poke_id in wanted and 
-            (last_notif is None or expiration > last_notif))
+        return (poke_id in wanted and
+               (last_notif is None or expiration > last_notif))
 
-    def empty (self):
+    def empty(self):
         status = self.get_status()
         number = self.get_phone_number()
         location = self.is_location_set()
         pokemon = self.is_pokemon_wanted_set()
         return not (status or number or location or pokemon)
 
-    def __str__ (self):
+    def __str__(self):
         template = ("Phone #: {}\n"
                     "Pokemon wanted: {}\n"
                     "Latitude: {}\n"
