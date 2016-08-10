42416360413f037d704bb7c2ea5b6fb0c4b142ca
Mark Lee <marklee.lhs@gmail.com>
2016-08-09 23:58:50 -0400

    Flatten directory structure a bit, make dev and prod more consistent

--- scripts/run_pending_redis.sh
+++ scripts/run_pending_redis.sh
@@ -5,7 +5,11 @@ source scripts/config.sh
 
 pid_redis=$(get_pid $REDIS_PENDING_SERVICE)
 if [ ! -n "$pid_redis" ]; then
-  nohup service $REDIS_PENDING_SERVICE start >$REDIS_PENDING_LOG_FILE 2>&1 &
+  if [ $PNU_ENV = "prod" ]; then
+    nohup service $REDIS_PENDING_SERVICE start >$REDIS_PENDING_LOG_FILE 2>&1 &
+  else
+    nohup $REDIS_PENDING_SERVICE $REDIS_PENDING_CONF_FILE >$REDIS_PENDING_LOG_FILE 2>&1 &
+  fi
 else
   echo -e "${RED}redis-pending already running. Try [kill -9 $pid_redis] first, then re-run.${NC}"
 fi
--- scripts/run_user_redis.sh
+++ scripts/run_user_redis.sh
@@ -5,7 +5,11 @@ source scripts/config.sh
 
 pid_redis=$(get_pid $REDIS_USER_SERVICE)
 if [ ! -n "$pid_redis" ]; then
-  nohup service $REDIS_USER_SERVICE start >$REDIS_USER_LOG_FILE 2>&1 &
+  if [ $PNU_ENV = "prod" ]; then
+    nohup service $REDIS_USER_SERVICE start >$REDIS_USER_LOG_FILE 2>&1 &
+  else
+    nohup $REDIS_USER_SERVICE $REDIS_USER_CONF_FILE >$REDIS_USER_LOG_FILE 2>&1 &
+  fi
 else
   echo -e "${RED}redis-user already running. Try [kill -9 $pid_redis] first, then re-run.${NC}"
 fi
