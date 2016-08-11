d32d63dc9bcb7f8875bcd7f02357a9a2e10a732d
Mark Lee <marklee.lhs@gmail.com>
2016-08-11 01:51:54 -0400

    Use config vars in check_status, display helpful message if no pids found

--- scripts/check_status.sh
+++ scripts/check_status.sh
@@ -1,8 +1,15 @@
+source scripts/config.sh
+
 if [ -x "$(command -v service)" ]; then
   echo "Services:"
-  service --status-all | grep redis-pnu
+  service --status-all | grep -e $REDIS_PENDING_SERVICE -e $REDIS_USER_SERVICE
   echo
 fi
 
 printf "PIDs:\n"
-echo "`ps -fe | grep -e main.py -e redis-server | grep -v grep`"
+pids=`ps -fe | grep -e $MAIN_PROCESS -e $REDIS_PROCESS | grep -v grep`
+if [ -n "$pids" ]; then
+  echo "$pids"
+else
+  echo "No pids found."
+fi
